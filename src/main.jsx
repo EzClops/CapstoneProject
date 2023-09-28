d7c31cedbdba9e4d9be20d9aa22d1222092d4212
Jeramiha Holt <jeramihaholt04@gmail.com>
2023-09-27 23:37:12 -0700

    added file to build

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import Login from './components/Login.jsx'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 )
