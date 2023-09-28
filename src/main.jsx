8f057825079a65f5124acb63654096ea282acde1
Jeramiha Holt <jeramihaholt04@gmail.com>
2023-09-27 23:53:42 -0700

    added file to build

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/CapstoneProject'>
+    <BrowserRouter>
       <App />
       <Routes>
         <Route path='/login' element={<Login/>}/>
