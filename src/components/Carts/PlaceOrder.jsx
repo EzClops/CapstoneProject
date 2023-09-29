2d9e8486e3d8da19078900fd9d2c40756728dd14
Jeramiha Holt <jeramihaholt04@gmail.com>
2023-09-28 20:47:23 -0700

    more changes

--- src/components/Carts/PlaceOrder.jsx
+++ src/components/Carts/PlaceOrder.jsx
@@ -1,6 +1,9 @@
 export default function PlaceOrder(){
     const userCartId = 1;
-
+    JSON.parse(localStorage.getItem(`All_Products_In_User_Cart${userCartId}`)).map(product =>{
+        localStorage.removeItem(`productId:${product["productId"]}[${userCartId}]`)
+        localStorage.removeItem(`ProductTotalPrice_${product["productId"]}`)
+    })
     localStorage.setItem(`All_Products_In_User_Cart${userCartId}`, '[]')
     return(
         <>
